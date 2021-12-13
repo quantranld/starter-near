9bbbc2bdb74a1a906cfde787d9e4417674c4eb1f
Quan <qmenq07@gmail.com>
2021-12-13 20:36:27 +0700

    fix: add init fn

--- src/singleton/assembly/index.ts
+++ src/singleton/assembly/index.ts
@@ -4,6 +4,8 @@ import { storage, Context } from "near-sdk-core"
 export class Contract {
   private message: string = 'hello world'
 
+  init() : void {}
+
   // return the string 'hello world'
   helloWorld(): string {
     return this.message
